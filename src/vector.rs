d7285551338ded77a04bcb8bbfc1c37ced1ee99e
Dzmitry Malyshau <kvarkus@gmail.com>
2017-05-31 15:33:42 -0400

    Macro generating vectors and points

--- src/vector.rs
+++ src/vector.rs
@@ -1,86 +1,50 @@
-/// Vector in 2D space.
-#[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
-#[repr(C)]
-pub struct Vector2<T> {
-    /// X component of a vector.
-    pub x: T,
-    /// Y component of a vector.
-    pub y: T,
-}
+macro_rules! vec {
+    ($name:ident [ $($field:ident = $index:expr),* ] = $fixed:ty) => {
+        #[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
+        #[repr(C)]
+        #[allow(missing_docs)] //TODO: actually have docs
+        pub struct $name<T> {
+            $( pub $field : T, )*
+        }
 
-impl<T: Clone> From<[T; 2]> for Vector2<T> {
-    fn from(v: [T; 2]) -> Self {
-        Vector2 {
-            x: v[0].clone(),
-            y: v[1].clone(),
+        impl<T: Clone> From<$fixed> for $name<T> {
+            fn from(v: $fixed) -> Self {
+                $name {
+                    $(
+                        $field: v[$index].clone(),
+                    )*
+                }
+            }
+        }
+
+        impl<T> Into<$fixed> for $name<T> {
+            fn into(self) -> $fixed {
+                [$( self.$field.into() ),*]
+            }
         }
     }
 }
 
-impl<T> Into<[T; 2]> for Vector2<T> {
-    fn into(self) -> [T; 2] {
-        [self.x, self.y]
-    }
-}
-
-
-/// Vector in 3D space.
-#[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
-#[repr(C)]
-pub struct Vector3<T> {
-    /// X component of a vector.
-    pub x: T,
-    /// Y component of a vector.
-    pub y: T,
-    /// Z component of a vector.
-    pub z: T,
-}
-
-impl<T: Clone> From<[T; 3]> for Vector3<T> {
-    fn from(v: [T; 3]) -> Self {
-        Vector3 {
-            x: v[0].clone(),
-            y: v[1].clone(),
-            z: v[2].clone(),
+macro_rules! from {
+    ($name:ident [ $($field:ident),* ] = $other:ident) => {
+        impl<T> From<$other<T>> for $name<T> {
+            fn from(v: $other<T>) -> Self {
+                $name {
+                    $(
+                        $field: v.$field,
+                    )*
+                }
+            }
         }
     }
 }
 
-impl<T> Into<[T; 3]> for Vector3<T> {
-    fn into(self) -> [T; 3] {
-        [self.x, self.y, self.z]
-    }
-}
-
-
-/// Vector in 4D space.
-/// Useful as a homogeneous 3D vector representation.
-#[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
-#[repr(C)]
-pub struct Vector4<T> {
-    /// X component of a vector.
-    pub x: T,
-    /// Y component of a vector.
-    pub y: T,
-    /// Z component of a vector.
-    pub z: T,
-    /// W component of a vector.
-    pub w: T,
-}
-
-impl<T: Clone> From<[T; 4]> for Vector4<T> {
-    fn from(v: [T; 4]) -> Self {
-        Vector4 {
-            x: v[0].clone(),
-            y: v[1].clone(),
-            z: v[2].clone(),
-            w: v[3].clone(),
-        }
-    }
-}
-
-impl<T> Into<[T; 4]> for Vector4<T> {
-    fn into(self) -> [T; 4] {
-        [self.x, self.y, self.z, self.w]
-    }
-}
+vec!( Vector2 [x=0, y=1] = [T; 2] );
+from!( Vector2 [x,y] = Point2 );
+vec!( Vector3 [x=0, y=1, z=2] = [T; 3] );
+from!( Vector3 [x,y,z] = Point3 );
+vec!( Vector4 [x=0, y=1, z=2, w=3] = [T; 4] );
+vec!( Point2 [x=0, y=1] = [T; 2] );
+from!( Point2 [x,y] = Vector2 );
+vec!( Point3 [x=0, y=1, z=2] = [T; 3] );
+from!( Point3 [x,y,z] = Vector3 );
