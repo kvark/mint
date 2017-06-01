4d7527550240f8c06f2fb00f11ee77da75846cb3
Dzmitry Malyshau <kvarkus@gmail.com>
2017-06-01 16:15:43 -0400

    Split and clarified Euler angles

--- src/rotation.rs
+++ src/rotation.rs
@@ -1,9 +1,12 @@
 use vector::Vector3;
 
 
-/// Euler rotation angles in 3D space.
+/// Tait-Bryan rotation angles in ZYX convention, that is:
+///   - first, rotation around Z axis is done
+///   - next, rotation around the new Y axis is done
+///   - finally, rotation around the new X axis is done
 #[repr(C)]
-pub struct EulerAngles<T> {
+pub struct TaitBryanAngles<T> {
     /// Angle of rotation around X axis in range [-pi, pi] (_pitch_).
     pub x: T,
     /// Angle of rotation around Y axis in range [-pi/2, pi/2] (_yaw_).
@@ -12,9 +15,9 @@ pub struct EulerAngles<T> {
     pub z: T,
 }
 
-impl<T: Clone> From<[T; 3]> for EulerAngles<T> {
+impl<T: Clone> From<[T; 3]> for TaitBryanAngles<T> {
     fn from(v: [T; 3]) -> Self {
-        EulerAngles {
+        TaitBryanAngles {
             x: v[0].clone(),
             y: v[1].clone(),
             z: v[2].clone(),
@@ -22,9 +25,40 @@ impl<T: Clone> From<[T; 3]> for EulerAngles<T> {
     }
 }
 
-impl<T> Into<[T; 3]> for EulerAngles<T> {
+impl<T> Into<[T; 3]> for TaitBryanAngles<T> {
     fn into(self) -> [T; 3] {
         [self.x, self.y, self.z]
+    }
+}
+
+
+/// Proper Euler rotation angles in ZXZ convention, that is:
+///   - first, rotation around Z axis is done
+///   - next, rotation around the original X axis is done
+///   - finally, rotation around the original Z axis is done again
+#[repr(C)]
+pub struct ProperEulerAngles<T> {
+    /// Angle of rotation around Z axis in range [-pi, pi].
+    pub alpha: T,
+    /// Angle of rotation around X axis in range [-pi/2, pi/2].
+    pub beta: T,
+    /// Angle of rotation around Z axis in range [-pi, pi].
+    pub gamma: T,
+}
+
+impl<T: Clone> From<[T; 3]> for ProperEulerAngles<T> {
+    fn from(v: [T; 3]) -> Self {
+        ProperEulerAngles {
+            alpha: v[0].clone(),
+            beta: v[1].clone(),
+            gamma: v[2].clone(),
+        }
+    }
+}
+
+impl<T> Into<[T; 3]> for ProperEulerAngles<T> {
+    fn into(self) -> [T; 3] {
+        [self.alpha, self.beta, self.gamma]
     }
 }
 
