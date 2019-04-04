8ce9b4e5e2fe4c72d9c4eb80af0383dbbd21abf7
George Burton <burtonageo@gmail.com>
2019-04-04 12:19:16 +0100

    Remove clone/copy bounds on From<array> impls by using pattern matching

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,8 +1,8 @@
 use vector::{Vector2, Vector3, Vector4};
-use std::{mem, ptr};
+use std::mem;
 
 macro_rules! matrix {
-    ($name:ident : $vec:ident[ $($field:ident = $index:expr),* ] = ($inner:expr, $outer:expr)) => {
+    ($name:ident : $vec:ident[ $($field:ident[$($sub:ident),*] = $index:expr),* ] = ($inner:expr, $outer:expr)) => {
         #[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
         #[repr(C)]
         #[allow(missing_docs)] //TODO: actually have docs
@@ -10,12 +10,11 @@ macro_rules! matrix {
             $( pub $field : $vec<T>, )*
         }
 
-        //TODO: revise the `Copy` bound here
-        impl<T: Copy> From<[[T; $inner]; $outer]> for $name<T> {
-            fn from(m: [[T; $inner]; $outer]) -> Self {
+        impl<T> From<[[T; $inner]; $outer]> for $name<T> {
+            fn from([$($field),*]: [[T; $inner]; $outer]) -> Self {
                 $name {
                     $(
-                        $field: m[$index].into(),
+                        $field: From::from($field),
                     )*
                 }
             }
@@ -41,16 +40,12 @@ macro_rules! matrix {
             }
         }
 
-        //TODO: revise the `Copy` bound here
-        impl<T: Copy> Into<[T; $inner * $outer]> for $name<T> {
+        impl<T> Into<[T; $inner * $outer]> for $name<T> {
             fn into(self) -> [T; $inner * $outer] {
-                unsafe {
-                    let mut out: [T; $inner * $outer] = mem::uninitialized();
-                    $(
-                        ptr::copy_nonoverlapping(self.$field.as_ref().as_ptr(), out[$index*$inner..($index+1)*$inner].as_mut_ptr(), $inner);
-                    )*
-                    out
-                }
+                let $name { $($field),* } = self;
+                [
+                    $( $( $field.$sub ),* ),*
+                ]
             }
         }
 
@@ -79,55 +74,55 @@ macro_rules! turn {
 }
 
 /// 2x2 row-major matrix.
-matrix!( RowMatrix2 : Vector2[x=0,y=1] = (2, 2));
+matrix!( RowMatrix2 : Vector2[x[x,y]=0,y[x,y]=1] = (2, 2));
 turn!( RowMatrix2 : Vector2[x[x,y],y[x,y]] = ColumnMatrix2 );
 /// 2x3 row-major matrix.
 /// Useful for combining rotation, scale, and translation in 2D space.
-matrix!( RowMatrix2x3 : Vector3[x=0,y=1] = (3, 2));
+matrix!( RowMatrix2x3 : Vector3[x[x,y,z]=0,y[x,y,z]=1] = (3, 2));
 turn!( RowMatrix2x3 : Vector3[x[x,y,z],y[x,y,z]] = ColumnMatrix2x3 );
 /// 3x2 row-major matrix.
 /// Useful for combining rotation, scale, and translation in 2D space.
-matrix!( RowMatrix3x2 : Vector2[x=0,y=1,z=2] = (2, 3));
+matrix!( RowMatrix3x2 : Vector2[x[x,y]=0,y[x,y]=1,z[x,y]=2] = (2, 3));
 turn!( RowMatrix3x2 : Vector2[x[x,y],y[x,y],z[x,y]] = ColumnMatrix3x2 );
 /// 3x3 row-major matrix.
 /// Useful for representing rotation and scale in 3D space.
-matrix!( RowMatrix3 : Vector3[x=0,y=1,z=2] = (3, 3));
+matrix!( RowMatrix3 : Vector3[x[x,y,z]=0,y[x,y,z]=1,z[x,y,z]=2] = (3, 3));
 turn!( RowMatrix3 : Vector3[x[x,y,z],y[x,y,z],z[x,y,z]] = ColumnMatrix3 );
 /// 3x4 row-major matrix.
 /// Useful for combining rotation, scale, and translation in 3D space.
-matrix!( RowMatrix3x4 : Vector4[x=0,y=1,z=2] = (4, 3));
+matrix!( RowMatrix3x4 : Vector4[x[x,y,z,w]=0,y[x,y,z,w]=1,z[x,y,z,w]=2] = (4, 3));
 turn!( RowMatrix3x4 : Vector4[x[x,y,z,w],y[x,y,z,w],z[x,y,z,w]] = ColumnMatrix3x4 );
 /// 4x3 row-major matrix.
 /// Useful for combining rotation, scale, and translation in 3D space.
-matrix!( RowMatrix4x3 : Vector3[x=0,y=1,z=2,w=3] = (3, 4));
+matrix!( RowMatrix4x3 : Vector3[x[x,y,z]=0,y[x,y,z]=1,z[x,y,z]=2,w[x,y,z]=3] = (3, 4));
 turn!( RowMatrix4x3 : Vector3[x[x,y,z],y[x,y,z],z[x,y,z],w[x,y,z]] = ColumnMatrix4x3 );
 /// 4x4 row-major matrix.
-matrix!( RowMatrix4 : Vector4[x=0,y=1,z=2,w=3] = (4, 4));
+matrix!( RowMatrix4 : Vector4[x[x,y,z,w]=0,y[x,y,z,w]=1,z[x,y,z,w]=2,w[x,y,z,w]=3] = (4, 4));
 turn!( RowMatrix4 : Vector4[x[x,y,z,w],y[x,y,z,w],z[x,y,z,w],w[x,y,z,w]] = ColumnMatrix4 );
 
 /// 2x2 column-major matrix.
-matrix!( ColumnMatrix2 : Vector2[x=0,y=1] = (2, 2));
+matrix!( ColumnMatrix2 : Vector2[x[x,y]=0,y[x,y]=1] = (2, 2));
 turn!( ColumnMatrix2 : Vector2[x[x,y],y[x,y]] = RowMatrix2 );
 /// 2x3 column-major matrix.
 /// Useful for combining rotation, scale, and translation in 2D space.
-matrix!( ColumnMatrix2x3 : Vector2[x=0,y=1,z=2] = (2, 3));
+matrix!( ColumnMatrix2x3 : Vector2[x[x,y]=0,y[x,y]=1,z[x,y]=2] = (2, 3));
 turn!( ColumnMatrix2x3 : Vector2[x[x,y],y[x,y],z[x,y]] = RowMatrix2x3 );
 /// 3x2 column-major matrix.
 /// Useful for combining rotation, scale, and translation in 2D space.
-matrix!( ColumnMatrix3x2 : Vector3[x=0,y=1] = (3, 2));
+matrix!( ColumnMatrix3x2 : Vector3[x[x,y,z]=0,y[x,y,z]=1] = (3, 2));
 turn!( ColumnMatrix3x2 : Vector3[x[x,y,z],y[x,y,z]] = RowMatrix3x2 );
 /// 3x3 column-major matrix.
 /// Useful for representing rotation and scale in 3D space.
-matrix!( ColumnMatrix3 : Vector3[x=0,y=1,z=2] = (3, 3));
+matrix!( ColumnMatrix3 : Vector3[x[x,y,z]=0,y[x,y,z]=1,z[x,y,z]=2] = (3, 3));
 turn!( ColumnMatrix3 : Vector3[x[x,y,z],y[x,y,z],z[x,y,z]] = RowMatrix3 );
 /// 3x4 column-major matrix.
 /// Useful for combining rotation, scale, and translation in 3D space.
-matrix!( ColumnMatrix3x4 : Vector3[x=0,y=1,z=2,w=3] = (3, 4));
+matrix!( ColumnMatrix3x4 : Vector3[x[x,y,z]=0,y[x,y,z]=1,z[x,y,z]=2,w[x,y,z]=3] = (3, 4));
 turn!( ColumnMatrix3x4 : Vector3[x[x,y,z],y[x,y,z],z[x,y,z],w[x,y,z]] = RowMatrix3x4 );
 /// 4x3 column-major matrix.
 /// Useful for combining rotation, scale, and translation in 3D space.
-matrix!( ColumnMatrix4x3 : Vector4[x=0,y=1,z=2] = (4, 3));
+matrix!( ColumnMatrix4x3 : Vector4[x[x,y,z,w]=0,y[x,y,z,w]=1,z[x,y,z,w]=2] = (4, 3));
 turn!( ColumnMatrix4x3 : Vector4[x[x,y,z,w],y[x,y,z,w],z[x,y,z,w]] = RowMatrix4x3 );
 /// 4x4 column-major matrix.
-matrix!( ColumnMatrix4 : Vector4[x=0,y=1,z=2,w=3] = (4, 4));
+matrix!( ColumnMatrix4 : Vector4[x[x,y,z,w]=0,y[x,y,z,w]=1,z[x,y,z,w]=2,w[x,y,z,w]=3] = (4, 4));
 turn!( ColumnMatrix4 : Vector4[x[x,y,z,w],y[x,y,z,w],z[x,y,z,w],w[x,y,z,w]] = RowMatrix4 );
