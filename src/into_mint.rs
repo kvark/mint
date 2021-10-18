d023aa66871979c23189cace5c2347e5936c09b1
Lucien Greathouse <me@lpghatguy.com>
2021-10-18 17:34:23 -0400

    Remove IntoMint implementation for primitives

--- src/into_mint.rs
+++ src/into_mint.rs
@@ -7,17 +7,3 @@ pub trait IntoMint: Into<Self::MintType> {
         self.into()
     }
 }
-
-macro_rules! into_mint_identity {
-    ($($ty:ident),*) => {
-        $(
-            impl IntoMint for $ty {
-                type MintType = $ty;
-            }
-        )*
-    };
-}
-
-into_mint_identity!(f32, f64);
-into_mint_identity!(u8, u16, u32, u64, u128);
-into_mint_identity!(i8, i16, i32, i64, i128);
